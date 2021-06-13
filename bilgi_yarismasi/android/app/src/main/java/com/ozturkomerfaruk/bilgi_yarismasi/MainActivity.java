051a1ed523c59cc7871c6b355b027d3eac99d282
Omer Faruk Ozturk <152120171018@ogrenci.ogu.edu.tr>
2021-06-13 12:57:05 +0300

    4. Çalışma

---
+++ bilgi_yarismasi/android/app/src/main/java/com/ozturkomerfaruk/bilgi_yarismasi/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ozturkomerfaruk.bilgi_yarismasi;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ bilgi_yarismasi/lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Icon dogruYanitIconu = Icon(Icons.mood, color: Colors.green, size: 30);
+const Icon yanlisYanitIconu = Icon(Icons.mood_bad, color: Colors.red, size: 30);
